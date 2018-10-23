c492e35e41580faf273f010d1b80a9cb905657b3
Giorgos <mintzopg@users.noreply.github.com>
2018-10-23 16:55:17 +0300

    Create main.go
    working on it

---
+++ ClimbeLeaderBoard/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type player struct {
+	score int32
+	rank  int
+}
+
+type rankings []player
+
+func climbingLeaderboard(scores []int32, alice []int32) []int32 {
+	res := []int32{} // keep alice position per game
+
+	// return result
+	return res
+}
+
+func main() {
+	fmt.Println()
+}
