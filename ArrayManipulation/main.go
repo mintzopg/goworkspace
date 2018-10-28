78bee1a96a5954c771c5c0ab665ff56c2951c7ea
Giorgos Mintzopoulos <gimin@outlook.com>
2018-10-28 13:39:13 +0200

    passed all test cases

--- ArrayManipulation/main.go
+++ ArrayManipulation/main.go
@@ -1,34 +1,49 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
-func arrayManipulation(n int, queries [][]int) int64 {
-	// queries is a 2-D list of operation Arr[m][3]
-	m := len(queries)
-	v := make([]int, n)
-	for i := range v {
-		v[i] = 0
+// using difference array of an array concept, to do some array manipulations
+
+func arrayManipulation(n int32, queries [][]int32) int64 {
+	A := make([]int, n)   // initial array of zeros for this case, to be updated
+	D := make([]int, n+1) // this will also be an array of zeroes for this specific case
+
+	for y := 0; y < len(queries); y++ {
+		l := queries[y][0] - 1
+		r := queries[y][1] - 1
+		x := queries[y][2]
+		update(D, int(l), int(r), int(x))
 	}
-
-	for y := 0; y < m; y++ {
-		a := queries[y][0]
-		b := queries[y][1]
-		k := queries[y][2]
-		// fmt.Println(a, b, k)
-		for j := a - 1; j <= b-1; j++ {
-			v[j] += k
+	reconstruct(A, D)
+	max := A[0]
+	for _, val := range A[1:] { // O(n)
+		if val > max {
+			max = val
 		}
 	}
-	sort.Ints(v)
-	fmt.Println(v)
-	return int64(v[n-1])
+	return int64(max)
+}
+
+func reconstruct(A []int, D []int) { // O(n)
+	n := len(A)
+	for i := 0; i < n; i++ {
+		if i == 0 {
+			A[i] = D[i]
+		} else {
+			A[i] = D[i] + A[i-1]
+		}
+	}
+	fmt.Println(A)
+}
+
+func update(D []int, l, r, x int) {
+	D[l] += x
+	D[r+1] -= x
 }
 
 func main() {
-	n := 5
-	arr := [][]int{[]int{1, 2, 100}, []int{2, 5, 100}, []int{3, 4, 100}}
-	fmt.Println(arrayManipulation(n, arr))
+	var n int32 = 5
+	queries := [][]int32{[]int32{1, 2, 100}, []int32{2, 5, 100}, []int32{3, 4, 100}}
+	res := arrayManipulation(n, queries)
+	fmt.Println(res)
 }
