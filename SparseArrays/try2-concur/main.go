ab8ea92db2275858bac3c2aa128d50a8910d91d0
Giorgos Mintzopoulos <gimin@outlook.com>
2018-11-11 13:39:23 +0200

    done

--- SparseArrays/try2-concur/main.go
+++ SparseArrays/try2-concur/main.go
@@ -6,23 +6,24 @@ import (
 	"sync"
 )
 
-var wg sync.WaitGroup
-
 func matchingStrings(strings []string, queries []string) []int32 {
+	var wg sync.WaitGroup
+
 	freq := make([]int32, len(queries))
 
-	for i, query := range queries {
+	for i, query := range queries { // launch len(queries) number of concurrent go routines
 		wg.Add(1)
-		go contains(strings, query, freq, i)
+		go contains(strings, query, freq, i, &wg)
 	}
+	wg.Wait()
 	return freq
 }
 
-func contains(sls []string, qs string, freq []int32, index int) {
+func contains(sls []string, qs string, freq []int32, index int, wg *sync.WaitGroup) {
+	// fmt.Println("go routine #", runtime.NumGoroutine())
 	for _, s := range sls {
 		if strings.Contains(s, qs) && len(s) == len(qs) {
-			freq[index]++
-			fmt.Printf("freq:= %v\n", freq)
+			freq[index]++ // freq[index] is not shared memory location => no race here
 		}
 	}
 	wg.Done()
