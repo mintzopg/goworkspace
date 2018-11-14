f4f8f1eeb593f04004ed75a1030c0864c2c69d2b
Giorgos Mintzopoulos <gimin@outlook.com>
2018-11-14 20:40:31 +0200

    in progress

--- NewYearChaos.HR/main.go
+++ NewYearChaos.HR/main.go
@@ -1,32 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 // Hacker Rank problem
 
-type person struct {
-	t0 int32 // initial position
-	ti int32 // position now
-	bn int32 // number of bribes
-}
-
 func minimumBribes(q []int32) {
-	var persons = []person{}
-
-	for i, val := range q {
-		j := int32(i) + 1
-		persons = append(persons, person{
-			t0: val,
-			ti: j,
-			bn: int32(math.Abs(float64(val - j))),
-		})
+	q0 := make([]int32, len(q))
+	var cost int32
+	for i := range q {
+		q0[i] = int32(i + 1)
 	}
-	fmt.Printf("persons: %v\n", persons)
+	for i, v := range q {
+		// check unmoved
+		if q0[i] == v {
+			continue
+		}
+		diff := v - int32(i+1)
+		if diff < 0 {
+			continue
+		}
+		if diff >= 1 && diff <= 2 {
+			cost += diff
+		} else {
+			fmt.Println("Too chaotic")
+			return
+		}
+	}
+
+	fmt.Println(cost)
 }
 func main() {
-	queue := []int32{1, 2, 3, 5, 4, 7, 6, 8}
+	// queue := []int32{2, 1, 5, 3, 4}    // OK (3)
+	// queue := []int32{2, 5, 1, 3, 4}  // OK (Too chaotic)
+	// queue := []int32{5, 1, 2, 3, 7, 8, 6, 4} // OK (Too chaotic)
+	queue := []int32{1, 2, 5, 3, 7, 8, 6, 4}
 	minimumBribes(queue)
 }
